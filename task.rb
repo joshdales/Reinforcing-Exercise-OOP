292b0cb09c4b6f61236c33e6086bee598596527e
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-29 13:02:13 -0400

    Write getter and setter methods for @description

--- task.rb
+++ task.rb
@@ -5,6 +5,12 @@ def initialize(description, due_date)
   @due_date = due_date
 end
 
+def description
+  @description
+end
 
+def description=(new_description)
+  @description = new_description
+end
 
 end
