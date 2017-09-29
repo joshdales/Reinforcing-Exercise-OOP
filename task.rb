13def6b87a06704b0499d59bfec3e51ead7efd46
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-29 13:10:18 -0400

    Create TodoList class that can ad new tasks to it

--- task.rb
+++ task.rb
@@ -1,24 +1,39 @@
 class Task
 
-def initialize(description, due_date)
-  @description = description
-  @due_date = due_date
+  def initialize(description, due_date)
+    @description = description
+    @due_date = due_date
+  end
+
+  def description
+    @description
+  end
+
+  def description=(new_description)
+    @description = new_description
+  end
+
+  def due_date
+    @due_date
+  end
+
+  def due_date=(new_due_date)
+    @due_date = new_due_date
+  end
 end
 
-def description
-  @description
-end
+class TodoList
+  @@tasks = []
+  def initialize(description, due_date)
+    task = Task.new(description, due_date)
+    @@tasks << tasks
+    task
+  end
 
-def description=(new_description)
-  @description = new_description
-end
-
-def due_date
-  @due_date
-end
-
-def due_date=(new_due_date)
-  @due_date = new_due_date
-end
+  def add_task(description, due_date)
+    new_task = Task.new(description, due_date)
+    @@tasks << new_task
+    new_task
+  end
 
 end
